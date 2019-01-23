16772ae5c3eefe7a9f699478c708f6614ffdc2d9
Federico Boschini <federico@nearit.com>
2019-01-23 13:29:55 +0100

    refactor(sample): start process on button click

--- app/src/main/java/com/nearit/pokeflute/sample/MainActivity.java
+++ app/src/main/java/com/nearit/pokeflute/sample/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nearit.pokeflute.SolutionActivity;
@@ -16,13 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 999;
 
+    TextView thankYou;
+    Button check;
+
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        if (Utils.isAppPotentiallyBlockedByManufacturer()) {
-            startActivityForResult(SolutionActivity.createIntent(this), REQUEST_CODE);
-        }
+        check = findViewById(R.id.button);
+        thankYou = findViewById(R.id.thankYou);
 
         // You can even enforce check
         if (Utils.checkForAppBlockersExplicitly(this)) {
@@ -30,9 +36,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onButtonClick(View view) {
+        if (Utils.isAppPotentiallyBlockedByManufacturer()) {
+            startActivityForResult(SolutionActivity.createIntent(this), REQUEST_CODE);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        thankYou.setVisibility(View.VISIBLE);
+
         if (requestCode == REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "Probably fixed", Toast.LENGTH_SHORT).show();
