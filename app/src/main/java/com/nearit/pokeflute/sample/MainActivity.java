0523e10d346541ae9da52375840b38d357b461d0
Federico Boschini <federico@nearit.com>
2019-01-23 11:59:31 +0100

    feat(sample): start SolutionActivity for result and watch activityResult

--- app/src/main/java/com/nearit/pokeflute/sample/MainActivity.java
+++ app/src/main/java/com/nearit/pokeflute/sample/MainActivity.java
@@ -1,8 +1,10 @@
 package com.nearit.pokeflute.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.nearit.pokeflute.SolutionActivity;
 import com.nearit.pokeflute.Utils;
@@ -12,17 +14,31 @@ import com.nearit.pokeflute.Utils;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 999;
+
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
         if (Utils.isAppPotentiallyBlockedByManufacturer()) {
-            startActivity(SolutionActivity.createIntent(this));
+            startActivityForResult(SolutionActivity.createIntent(this), REQUEST_CODE);
         }
 
         // You can even enforce check
         if (Utils.checkForAppBlockersExplicitly(this)) {
             // eventually start SolutionActivity
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Probably fixed", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Dismissed. Not Fixed", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
